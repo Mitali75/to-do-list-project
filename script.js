1f5c8e04dbe516a2b13ce8b1d11548a01e189cca
Mitali75 <mitalirandive.0075@gmail.com>
2026-02-26 19:46:55 +0530

    Updated todo app with checkbox and improved design

--- script.js
+++ script.js
@@ -1,79 +1,32 @@
-// access input field
-const input = document.querySelector('#todo-input');
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
 
-// Listening to click event from "Add" button.
-document.querySelector('#submit').addEventListener('click', () => {
-  // value of the input field
-  const inputData = input.value;
-  input.value = "";
+    if (taskText === "") return;
 
-  // creating todo item element
-  const todo_el = document.createElement('div');
-  todo_el.classList.add('todo-item');
+    const li = document.createElement("li");
 
-  const todo_content_el = document.createElement('div');
-  todo_el.appendChild(todo_content_el);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-  const todo_input_el = document.createElement('input');
-  todo_input_el.classList.add('text');
-  todo_input_el.type = 'text';
-  todo_input_el.value = inputData;
-  todo_input_el.setAttribute('readonly', 'readonly');
+    const span = document.createElement("span");
+    span.textContent = taskText;
 
-  todo_content_el.appendChild(todo_input_el);
+    checkbox.addEventListener("change", function() {
+        li.classList.toggle("completed");
+    });
 
-  const todo_actions_el = document.createElement('div');
-  todo_actions_el.classList.add('action-items');
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = function() {
+        li.remove();
+    };
 
-  const todo_done_el = document.createElement('i');
-  todo_done_el.classList.add('fa-solid');
-  todo_done_el.classList.add('fa-check');
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
 
-  const todo_edit_el = document.createElement('i');
-  todo_edit_el.classList.add('fa-solid');
-  todo_edit_el.classList.add('fa-pen-to-square');
-  todo_edit_el.classList.add('edit');
+    document.getElementById("taskList").appendChild(li);
 
-  const todo_delete_el = document.createElement('i');
-  todo_delete_el.classList.add('fa-solid');
-  todo_delete_el.classList.add('fa-trash');
-
-  todo_actions_el.appendChild(todo_done_el)
-  todo_actions_el.appendChild(todo_edit_el);
-  todo_actions_el.appendChild(todo_delete_el);
-
-  todo_el.appendChild(todo_actions_el);
-  console.log(todo_el)
-  // add the todo-item to lists
-  document.querySelector('.todo-lists').appendChild(todo_el);
-
-  // done functionality
-  todo_done_el.addEventListener('click', () => {
-    todo_input_el.classList.add('done')
-    todo_el.removeChild(todo_actions_el);
-  })
-
-  // edit functionality
-  todo_edit_el.addEventListener('click', (e) => {
-    if (todo_edit_el.classList.contains("edit")) {
-      todo_edit_el.classList.remove("edit");
-      todo_edit_el.classList.remove("fa-pen-to-square");
-      todo_edit_el.classList.add("fa-x");
-      todo_edit_el.classList.add("save");
-      todo_input_el.removeAttribute("readonly");
-      todo_input_el.focus();
-    } else {
-      todo_edit_el.classList.remove("save");
-      todo_edit_el.classList.remove("fa-x");
-      todo_edit_el.classList.add("fa-pen-to-square");
-      todo_edit_el.classList.add("edit");
-      todo_input_el.setAttribute("readonly", "readonly");
-    }
-  });
-
-  // delete functionality
-  todo_delete_el.addEventListener('click', (e) => {
-    console.log(todo_el);
-    document.querySelector('.todo-lists').removeChild(todo_el);
-  });
-})
+    input.value = "";
+}
